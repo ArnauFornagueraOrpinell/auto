af91dd44da51bc55768816720b6fd6b39a198345
Galiat <101833226+ArnauFornagueraOrpinell@users.noreply.github.com>
2024-02-29 04:55:17 +0100

    Update facturae.sh
    asd

--- facturae.sh
+++ facturae.sh
@@ -12,7 +12,6 @@ odoo_version="$version.0"
 cd /opt/odoo/odoo$version-custom-addons
 
 git clone https://github.com/OCA/l10n-spain.git --branch $odoo_version
-mv l10n-spain/account_payment_partner .
 mv l10n-spain/l10n_es_partner .
 mv l10n-spain/l10n_es_facturae .
 mv l10n-spain/l10n_es_aeat .
